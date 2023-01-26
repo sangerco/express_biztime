12f530ea53c2a36dc43ad1845375de2d24166c2b
Dan Sanger <sangerco@yahoo.com>
2023-01-25 18:54:21 -0700

    updated company return data

--- routes/companies.js
+++ routes/companies.js
@@ -16,12 +16,14 @@ router.get('/', async (req, res, next) => {
 router.get('/:code', async (req, res, next) => {
     try {
         const { code } = req.params;
-        const results = await db.query(`SELECT * FROM companies WHERE code=$1`, [code])
-        const result = await db.query('SELECT * FROM invoices WHERE comp_code=$1', [code])
-        if (results.rows.length === 0) {
+        const compResults = await db.query(`SELECT * FROM companies WHERE code=$1`, [code])
+        const invResults = await db.query('SELECT * FROM invoices WHERE comp_code=$1', [code])
+        if (compResults.rows.length === 0) {
             throw new ExpressError(`Can't find company with code ${code}`, 404);
         }
-        return res.json({ company: results.rows[0], invoices: [result.rows]});
+        const company = compResults.rows[0];
+        company.invoices = invResults.rows
+        return res.json(company);
     } catch (e) {
         next(e);
     }
